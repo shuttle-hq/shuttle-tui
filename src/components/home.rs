963fb261aad08ae2971b525bd63afd4e24f40343
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2023-10-20 13:06:29 +0300

    style(fmt): remove opinionated formatter

--- src/components/home.rs
+++ src/components/home.rs
@@ -9,71 +9,87 @@ use tui_input::Input;
 
 use super::{Component, Frame};
 use crate::{
-  action::Action,
-  config::{Config, KeyBindings},
+    action::Action,
+    config::{Config, KeyBindings},
 };
 
 #[derive(Default)]
 pub struct Home {
-  show_help: bool,
-  pub action_tx: Option<UnboundedSender<Action>>,
-  config: Config,
-  pub input: Input,
-  pub text: Vec<String>,
+    show_help: bool,
+    pub action_tx: Option<UnboundedSender<Action>>,
+    config: Config,
+    pub input: Input,
+    pub text: Vec<String>,
 }
 
 impl Home {
-  pub fn new() -> Self {
-    Self::default()
-  }
+    pub fn new() -> Self {
+        Self::default()
+    }
 }
 
 impl Component for Home {
-  fn register_action_handler(&mut self, tx: UnboundedSender<Action>) -> Result<()> {
-    self.action_tx = Some(tx);
-    Ok(())
-  }
-
-  fn register_config_handler(&mut self, config: Config) -> Result<()> {
-    self.config = config;
-    Ok(())
-  }
-
-  fn update(&mut self, action: Action) -> Result<Option<Action>> {
-    match action {
-      Action::Tick => {},
-      Action::ToggleShowHelp => self.show_help = !self.show_help,
-      _ => {},
+    fn register_action_handler(&mut self, tx: UnboundedSender<Action>) -> Result<()> {
+        self.action_tx = Some(tx);
+        Ok(())
     }
-    Ok(None)
-  }
 
-  fn draw(&mut self, f: &mut Frame<'_>, area: Rect) -> Result<()> {
-    f.render_widget(Paragraph::new("hello world"), area);
+    fn register_config_handler(&mut self, config: Config) -> Result<()> {
+        self.config = config;
+        Ok(())
+    }
 
-    if self.show_help {
-      let rect = f.size().inner(&Margin { horizontal: 4, vertical: 2 });
-      f.render_widget(Clear, rect);
-      let block = Block::default()
-        .title(Line::from(vec![Span::styled("Key Bindings", Style::default().add_modifier(Modifier::BOLD))]))
-        .borders(Borders::ALL)
-        .border_style(Style::default().fg(Color::Yellow));
-      f.render_widget(block, rect);
-      let rows = vec![
-        Row::new(vec!["j", "Increment"]),
-        Row::new(vec!["k", "Decrement"]),
-        Row::new(vec!["/", "Enter Input"]),
-        Row::new(vec!["ESC", "Exit Input"]),
-        Row::new(vec!["Enter", "Submit Input"]),
-        Row::new(vec!["q", "Quit"]),
-        Row::new(vec!["?", "Open Help"]),
-      ];
-      let table = Table::new(rows)
-        .header(Row::new(vec!["Key", "Action"]).bottom_margin(1).style(Style::default().add_modifier(Modifier::BOLD)))
-        .widths(&[Constraint::Percentage(10), Constraint::Percentage(90)])
-        .column_spacing(1);
-      f.render_widget(table, rect.inner(&Margin { vertical: 4, horizontal: 2 }));
-    };
-    Ok(())
-  }
+    fn update(&mut self, action: Action) -> Result<Option<Action>> {
+        match action {
+            Action::Tick => {}
+            Action::ToggleShowHelp => self.show_help = !self.show_help,
+            _ => {}
+        }
+        Ok(None)
+    }
+
+    fn draw(&mut self, f: &mut Frame<'_>, area: Rect) -> Result<()> {
+        f.render_widget(Paragraph::new("hello world"), area);
+
+        if self.show_help {
+            let rect = f.size().inner(&Margin {
+                horizontal: 4,
+                vertical: 2,
+            });
+            f.render_widget(Clear, rect);
+            let block = Block::default()
+                .title(Line::from(vec![Span::styled(
+                    "Key Bindings",
+                    Style::default().add_modifier(Modifier::BOLD),
+                )]))
+                .borders(Borders::ALL)
+                .border_style(Style::default().fg(Color::Yellow));
+            f.render_widget(block, rect);
+            let rows = vec![
+                Row::new(vec!["j", "Increment"]),
+                Row::new(vec!["k", "Decrement"]),
+                Row::new(vec!["/", "Enter Input"]),
+                Row::new(vec!["ESC", "Exit Input"]),
+                Row::new(vec!["Enter", "Submit Input"]),
+                Row::new(vec!["q", "Quit"]),
+                Row::new(vec!["?", "Open Help"]),
+            ];
+            let table = Table::new(rows)
+                .header(
+                    Row::new(vec!["Key", "Action"])
+                        .bottom_margin(1)
+                        .style(Style::default().add_modifier(Modifier::BOLD)),
+                )
+                .widths(&[Constraint::Percentage(10), Constraint::Percentage(90)])
+                .column_spacing(1);
+            f.render_widget(
+                table,
+                rect.inner(&Margin {
+                    vertical: 4,
+                    horizontal: 2,
+                }),
+            );
+        };
+        Ok(())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,31 +3,31 @@ use cargo_shuttle::Shuttle;
 use clap::Parser;
 use color_eyre::eyre::{eyre, Result};
 use shuttle_tui::{
-  app::App,
-  args::Args,
-  utils::{initialize_logging, initialize_panic_handler},
+    app::App,
+    args::Args,
+    utils::{initialize_logging, initialize_panic_handler},
 };
 
 async fn tokio_main() -> Result<()> {
-  initialize_logging()?;
+    initialize_logging()?;
 
-  initialize_panic_handler()?;
+    initialize_panic_handler()?;
 
-  let args = Args::parse();
-  let shuttle = Shuttle::new().map_err(|e| eyre!("{e}"))?;
-  let mut app = App::new(shuttle, &args)?;
-  app.run().await?;
+    let args = Args::parse();
+    let shuttle = Shuttle::new().map_err(|e| eyre!("{e}"))?;
+    let mut app = App::new(shuttle, &args)?;
+    app.run().await?;
 
-  Ok(())
+    Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-  if let Err(e) = tokio_main().await {
-    eprintln!("{} error: Something went wrong", env!("CARGO_PKG_NAME"));
-    Err(e)
-  } else {
-    Ok(())
-  }
+    if let Err(e) = tokio_main().await {
+        eprintln!("{} error: Something went wrong", env!("CARGO_PKG_NAME"));
+        Err(e)
+    } else {
+        Ok(())
+    }
 }
 // ANCHOR_END: all
